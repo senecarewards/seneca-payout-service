f66e3f0a0d5bb59bfe6849d9ec08c28c286cafc8
senecarewards <deepakyadav355590@gmail.com>
2026-08-20 17:07:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ type: () => true }));
 
 const {
   PORT = 3000,
